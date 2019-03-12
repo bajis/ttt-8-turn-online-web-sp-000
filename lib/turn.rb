80db29cffdadfd9da0884bddd46cec4431c53a8a
Kelsey Bajis <bajis.simple.haven@gmail.com>
2019-03-12 05:34:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,13 +1,14 @@
-require_relative '../lib/turn'
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+# display board
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
 
-puts 'Welcometo Tic Tac Toe!'
+#converts user input to integer
 
-display_board(board)
-
-input = gets.strip
-input = input_to_index
-
-move(board, input)
-
-turn(board)
+def input_to_index(input)
+  input.to_i -1
+end
